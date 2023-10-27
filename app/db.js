06724b34f52494c5977fb16d9675ff54d5469f52
NattyNgrn <natnrgn@gmail.com>
2023-10-27 14:15:01 -0400

    set up for next js and prisma

---
+++ app/db.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from "@prisma/client";
+
+const { PrismaClient } = require('@prisma/client');
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+const globalForPrisma = globalThis;
+
+const prisma = globalForPrisma.prisma || prismaClientSingleton();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
+
+module.exports = prisma;
---
+++ prisma/migrations/20231027174356_init/migration.sql
@@ -0,0 +1,46 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "gender" TEXT NOT NULL,
+    "sexuality" TEXT NOT NULL,
+    "ethnicity" TEXT NOT NULL,
+    "race" TEXT NOT NULL,
+    "photourl" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "reviews" (
+    "id" SERIAL NOT NULL,
+    "rating" INTEGER NOT NULL,
+    "createdat" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "location" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "photosurl" TEXT NOT NULL,
+    "notes" TEXT,
+
+    CONSTRAINT "reviews_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "favorites" (
+    "id" SERIAL NOT NULL,
+    "location" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "favorites_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_username_key" ON "user"("username");
+
+-- AddForeignKey
+ALTER TABLE "reviews" ADD CONSTRAINT "reviews_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "favorites" ADD CONSTRAINT "favorites_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
