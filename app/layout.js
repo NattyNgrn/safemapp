70e32b10a43e79c30d896ed4f7077a43aaa2f309
NattyNgrn <natnrgn@gmail.com>
2023-10-27 16:51:47 -0400

    clerk working

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
 
 export const metadata = {
   title: 'Safe Map App',
@@ -7,8 +8,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`bg-cyan-900 container text-slate-200` }>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={`bg-cyan-900 container text-slate-200` }>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
