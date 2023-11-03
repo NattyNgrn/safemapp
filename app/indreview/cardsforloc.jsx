53552d9c3e41b40567a284c860a4b22edcdebd73
NattyNgrn <natnrgn@gmail.com>
2023-11-02 22:47:57 -0400

    UGH redeploying

--- app/indreview/cardsforloc.jsx
+++ app/indreview/cardsforloc.jsx
@@ -15,7 +15,7 @@ function Review(){
           </h5>
           <p class="mb-4 text-base text-neutral-600 dark:text-neutral-200">
             Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            bulk of the cards content.
           </p>
   
         </div>
